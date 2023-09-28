1322ce6ad35da8b6753ea617de208baa0746bbba
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-28 14:08:05 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,7 @@ const elToDoContentText = document.querySelector(".todo-content-text");
 const elToDoEdit = document.querySelector(".todo-edit");
 
 const elToDoDelete = document.querySelector(".todo-delete");
+
+let list = localStorage.getItem("keys")
+  ? JSON.parse(localStorage.getItem("keys"))
+  : [];
