854f80fe46726ea69533c710effa4c4fffcfb3af
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-28 14:05:44 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,5 @@ const elTemplate = document.querySelector("#template");
 const elToDoContent = document.querySelector(".todo-content");
 
 const elToDoContentText = document.querySelector(".todo-content-text");
+
+const elToDoEdit = document.querySelector(".todo-edit");
